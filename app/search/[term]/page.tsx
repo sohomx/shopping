a9a02b224a3838809a0e04925d4fded5f0ebf80e
Sohom Pal <sohom377@gmail.com>
2023-08-28 11:23:28 +0530

    wip: fetch failed

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -1,4 +1,5 @@
-import { SearchParams } from "@/typings";
+import { getFetchUrl } from "@/lib/getFetchUrl";
+import { PageResult, SearchParams } from "@/typings";
 import { redirect } from "next/navigation";
 
 type Props = {
@@ -8,12 +9,21 @@ type Props = {
   };
 };
 
-function SearchPage({ searchParams, params: { term } }: Props) {
+async function SearchPage({ searchParams, params: { term } }: Props) {
   if (!term) {
     redirect("/");
   }
 
   // fetch from API...
+  const response = await fetch(getFetchUrl("api/search"), {
+    method: "POST",
+    body: JSON.stringify({ searchTerm: term, ...searchParams }),
+  });
+
+  const results = (await response.json()) as PageResult[];
+
+  console.log(results);
+
   return <div>{/* ResultsList */}</div>;
 }
 
